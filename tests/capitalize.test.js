165cfd6f9cf5ca89819a62ab405e36f8bc2cfa0c
zavenchik <77801431+Zaven477@users.noreply.github.com>
2021-07-08 19:01:08 +0000

    update

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,13 @@
-import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+import capitalize from '../src/capitalize.js';
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
 
-console.log('Все тесты пройдены!');
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
+
+
+
+
+console.log('tests passed!');
+
